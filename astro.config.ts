123fa7b96ae18ff0435180c26a71c14b691fd5c6
y-honey <y.honey0128@gmail.com>
2025-03-05 17:25:20 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://makeinaai.github.io/website/', // Ensure this matches your GitHub Pages subdirectory
   base: '/website/', // Set the correct base path for GitHub Pages
-  outDir: 'dist', // Default output directory
+  output: 'static', // Default output directory
   build: {
-    assets: '_astro', // This should be correct, but GitHub Pages may ignore underscores
+    assets: 'assets', // This should be correct, but GitHub Pages may ignore underscores
   },
 });
